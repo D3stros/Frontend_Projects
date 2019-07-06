b0b527c1b3d1e959a7dece8df19a7efaa1123107
Destros <patrick.fallmann@hotmail.com>
2019-07-06 20:35:36 +0200

    Code beautified

--- GameOfThrones_Random_House_Picker/javascript/script.js
+++ GameOfThrones_Random_House_Picker/javascript/script.js
@@ -1,41 +1,41 @@
-function randomImage(){
+function randomImage() {
     var randomNumber = Math.floor((Math.random() * 6) + 1);
-	var el = document.getElementById("random-image-container");
-	if (randomNumber == 1) {
-    el.className = "Targaryen";
-    document.getElementById("targaryen").style.display = "block";
-    let para = document.getElementById("house_text");
-    para.innerText = "House" + " "+ el.className;
-    document.getElementById("tyrell").style.display = "none";
-    document.getElementById("lannister").style.display = "none";
-    document.getElementById("stark").style.display = "none";
-    document.getElementById("tully").style.display = "none";
-    document.getElementById("baratheon").style.display = "none";
-	} else if (randomNumber == 2) {
+    var el = document.getElementById("random-image-container");
+    if (randomNumber == 1) {
+        el.className = "Targaryen";
+        document.getElementById("targaryen").style.display = "block";
+        let para = document.getElementById("house_text");
+        para.innerText = "House" + " " + el.className;
+        document.getElementById("tyrell").style.display = "none";
+        document.getElementById("lannister").style.display = "none";
+        document.getElementById("stark").style.display = "none";
+        document.getElementById("tully").style.display = "none";
+        document.getElementById("baratheon").style.display = "none";
+    } else if (randomNumber == 2) {
         el.className = "Tyrell";
         let para = document.getElementById("house_text");
-        para.innerText = "House" + " "+ el.className;
+        para.innerText = "House" + " " + el.className;
         document.getElementById("tyrell").style.display = "block";
         document.getElementById("targaryen").style.display = "none";
         document.getElementById("lannister").style.display = "none";
         document.getElementById("stark").style.display = "none";
         document.getElementById("tully").style.display = "none";
         document.getElementById("baratheon").style.display = "none";
-	} else if (randomNumber == 3) {
+    } else if (randomNumber == 3) {
         el.className = "Lannister"
-        document.getElementById("lannister").style.display = "block"; 
+        document.getElementById("lannister").style.display = "block";
         let para = document.getElementById("house_text");
-        para.innerText = "House" + " "+ el.className;
+        para.innerText = "House" + " " + el.className;
         document.getElementById("tyrell").style.display = "none";
         document.getElementById("targaryen").style.display = "none";
         document.getElementById("stark").style.display = "none";
         document.getElementById("tully").style.display = "none";
         document.getElementById("baratheon").style.display = "none";
-	} else if (randomNumber == 4) {
+    } else if (randomNumber == 4) {
         el.className = "Stark"
         document.getElementById("stark").style.display = "block";
         let para = document.getElementById("house_text");
-        para.innerText = "House" + " "+ el.className;
+        para.innerText = "House" + " " + el.className;
         document.getElementById("tyrell").style.display = "none";
         document.getElementById("targaryen").style.display = "none";
         document.getElementById("lannister").style.display = "none";
@@ -43,9 +43,9 @@ function randomImage(){
         document.getElementById("baratheon").style.display = "none";
     } else if (randomNumber == 5) {
         el.className = "Tully"
-        document.getElementById("tully").style.display = "block"; 
+        document.getElementById("tully").style.display = "block";
         let para = document.getElementById("house_text");
-        para.innerText = "House" + " "+ el.className;
+        para.innerText = "House" + " " + el.className;
         document.getElementById("tyrell").style.display = "none";
         document.getElementById("targaryen").style.display = "none";
         document.getElementById("stark").style.display = "none";
@@ -55,7 +55,7 @@ function randomImage(){
         el.className = "Baratheon"
         document.getElementById("baratheon").style.display = "block";
         let para = document.getElementById("house_text");
-        para.innerText = "House" + " "+ el.className;
+        para.innerText = "House" + " " + el.className;
         document.getElementById("tyrell").style.display = "none";
         document.getElementById("targaryen").style.display = "none";
         document.getElementById("stark").style.display = "none";
@@ -63,5 +63,3 @@ function randomImage(){
         document.getElementById("lannister").style.display = "none";
     }
 }
-
-

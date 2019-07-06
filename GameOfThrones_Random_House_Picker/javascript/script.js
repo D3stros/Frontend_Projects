3c6680266f165f935b5f71d6b5069d2895a4a375
Destros <patrick.fallmann@hotmail.com>
2019-07-06 20:25:27 +0200

    Text functionality and svg documentation added.

--- GameOfThrones_Random_House_Picker/javascript/script.js
+++ GameOfThrones_Random_House_Picker/javascript/script.js
@@ -1,16 +1,22 @@
+
+
 function randomImage(){
+    var randomNumber = Math.floor((Math.random() * 6) + 1);
 	var el = document.getElementById("random-image-container");
-	var randomNumber = Math.floor((Math.random() * 6) + 1);
-	if (randomNumber == 1){
-    el.className = "random-image-01";
+	if (randomNumber == 1) {
+    el.className = "Targaryen";
     document.getElementById("targaryen").style.display = "block";
+    let para = document.getElementById("house_text");
+    para.innerText = "House" + " "+ el.className;
     document.getElementById("tyrell").style.display = "none";
     document.getElementById("lannister").style.display = "none";
     document.getElementById("stark").style.display = "none";
     document.getElementById("tully").style.display = "none";
     document.getElementById("baratheon").style.display = "none";
 	} else if (randomNumber == 2) {
-        el.className = "random-image-02";
+        el.className = "Tyrell";
+        let para = document.getElementById("house_text");
+        para.innerText = "House" + " "+ el.className;
         document.getElementById("tyrell").style.display = "block";
         document.getElementById("targaryen").style.display = "none";
         document.getElementById("lannister").style.display = "none";
@@ -18,32 +24,40 @@ function randomImage(){
         document.getElementById("tully").style.display = "none";
         document.getElementById("baratheon").style.display = "none";
 	} else if (randomNumber == 3) {
-        el.className = "random-image-03"
-        document.getElementById("lannister").style.display = "block";
+        el.className = "Lannister"
+        document.getElementById("lannister").style.display = "block"; 
+        let para = document.getElementById("house_text");
+        para.innerText = "House" + " "+ el.className;
         document.getElementById("tyrell").style.display = "none";
         document.getElementById("targaryen").style.display = "none";
         document.getElementById("stark").style.display = "none";
         document.getElementById("tully").style.display = "none";
         document.getElementById("baratheon").style.display = "none";
 	} else if (randomNumber == 4) {
-        el.className = "random-image-04"
+        el.className = "Stark"
         document.getElementById("stark").style.display = "block";
+        let para = document.getElementById("house_text");
+        para.innerText = "House" + " "+ el.className;
         document.getElementById("tyrell").style.display = "none";
         document.getElementById("targaryen").style.display = "none";
         document.getElementById("lannister").style.display = "none";
         document.getElementById("tully").style.display = "none";
         document.getElementById("baratheon").style.display = "none";
     } else if (randomNumber == 5) {
-        el.className = "random-image-05"
-        document.getElementById("tully").style.display = "block";
+        el.className = "Tully"
+        document.getElementById("tully").style.display = "block"; 
+        let para = document.getElementById("house_text");
+        para.innerText = "House" + " "+ el.className;
         document.getElementById("tyrell").style.display = "none";
         document.getElementById("targaryen").style.display = "none";
         document.getElementById("stark").style.display = "none";
         document.getElementById("lannister").style.display = "none";
         document.getElementById("baratheon").style.display = "none";
     } else {
-        el.className = "random-image-06"
+        el.className = "Baratheon"
         document.getElementById("baratheon").style.display = "block";
+        let para = document.getElementById("house_text");
+        para.innerText = "House" + " "+ el.className;
         document.getElementById("tyrell").style.display = "none";
         document.getElementById("targaryen").style.display = "none";
         document.getElementById("stark").style.display = "none";

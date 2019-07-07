d199ee0735ee9ec625c7c54667d2a6edbbc74124
Destros <patrick.fallmann@hotmail.com>
2019-07-07 21:19:38 +0200

    Javascript code shortened

--- GameOfThrones_Random_House_Picker/javascript/script.js
+++ GameOfThrones_Random_House_Picker/javascript/script.js
@@ -6,60 +6,49 @@ function randomImage() {
         document.getElementById("targaryen").style.display = "block";
         let para = document.getElementById("house_text");
         para.innerText = "House" + " " + el.className;
-        document.getElementById("tyrell").style.display = "none";
-        document.getElementById("lannister").style.display = "none";
-        document.getElementById("stark").style.display = "none";
-        document.getElementById("tully").style.display = "none";
-        document.getElementById("baratheon").style.display = "none";
+       var allHouses = document.querySelectorAll("#tyrell, #lannister, #stark, #tully, #baratheon").forEach(el => {
+           el.style.display = "none";
+       });
+       
     } else if (randomNumber == 2) {
         el.className = "Tyrell";
+        document.getElementById("tyrell").style.display = "block";
         let para = document.getElementById("house_text");
         para.innerText = "House" + " " + el.className;
-        document.getElementById("tyrell").style.display = "block";
-        document.getElementById("targaryen").style.display = "none";
-        document.getElementById("lannister").style.display = "none";
-        document.getElementById("stark").style.display = "none";
-        document.getElementById("tully").style.display = "none";
-        document.getElementById("baratheon").style.display = "none";
+        var allHouses = document.querySelectorAll("#targaryen, #lannister, #stark, #tully, #baratheon").forEach(el => {
+            el.style.display = "none";
+        });
     } else if (randomNumber == 3) {
         el.className = "Lannister"
         document.getElementById("lannister").style.display = "block";
         let para = document.getElementById("house_text");
         para.innerText = "House" + " " + el.className;
-        document.getElementById("tyrell").style.display = "none";
-        document.getElementById("targaryen").style.display = "none";
-        document.getElementById("stark").style.display = "none";
-        document.getElementById("tully").style.display = "none";
-        document.getElementById("baratheon").style.display = "none";
+        var allHouses = document.querySelectorAll("#tyrell, #targaryen, #stark, #tully, #baratheon").forEach(el => {
+            el.style.display = "none";
+        });
     } else if (randomNumber == 4) {
         el.className = "Stark"
         document.getElementById("stark").style.display = "block";
         let para = document.getElementById("house_text");
         para.innerText = "House" + " " + el.className;
-        document.getElementById("tyrell").style.display = "none";
-        document.getElementById("targaryen").style.display = "none";
-        document.getElementById("lannister").style.display = "none";
-        document.getElementById("tully").style.display = "none";
-        document.getElementById("baratheon").style.display = "none";
+        var allHouses = document.querySelectorAll("#tyrell, #lannister, #targaryen, #tully, #baratheon").forEach(el => {
+            el.style.display = "none";
+        });
     } else if (randomNumber == 5) {
         el.className = "Tully"
         document.getElementById("tully").style.display = "block";
         let para = document.getElementById("house_text");
         para.innerText = "House" + " " + el.className;
-        document.getElementById("tyrell").style.display = "none";
-        document.getElementById("targaryen").style.display = "none";
-        document.getElementById("stark").style.display = "none";
-        document.getElementById("lannister").style.display = "none";
-        document.getElementById("baratheon").style.display = "none";
+        var allHouses = document.querySelectorAll("#tyrell, #lannister, #stark, #targaryen, #baratheon").forEach(el => {
+            el.style.display = "none";
+        });
     } else {
         el.className = "Baratheon"
         document.getElementById("baratheon").style.display = "block";
         let para = document.getElementById("house_text");
         para.innerText = "House" + " " + el.className;
-        document.getElementById("tyrell").style.display = "none";
-        document.getElementById("targaryen").style.display = "none";
-        document.getElementById("stark").style.display = "none";
-        document.getElementById("tully").style.display = "none";
-        document.getElementById("lannister").style.display = "none";
+        var allHouses = document.querySelectorAll("#tyrell, #lannister, #stark, #tully, #targaryen").forEach(el => {
+            el.style.display = "none";
+        });
     }
 }
